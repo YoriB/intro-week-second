0e201571ea80a33b9f32ca9247a1e8ce10965a77
YoriB <deyoribad@yahoo.co.uk>
2022-11-29 11:28:42 +0000

    javascript intro almost complete

--- javascript-intro/questions/log-0.js
+++ javascript-intro/questions/log-0.js
@@ -1,1 +1,1 @@
-// console.log("Hello there northcoders!");
+console.log("Hello there northcoders!");
--- javascript-intro/questions/log-6.js
+++ javascript-intro/questions/log-6.js
@@ -1,1 +1,1 @@
-// console.log('Hello there northcoders!'.toUpperCase());
+ console.log('Hello there northcoders!'.toUpperCase());
