66b55b189a90c85b62a55bdc57f920adb465e443
YoriB <114592835+YoriB@users.noreply.github.com>
2022-12-13 12:04:32 +0000

    Add files via upload

---
+++ data-structures/queue.js
@@ -0,0 +1,10 @@
+function createQueue(maxSize) {
+  // build your stack object inside this factory function
+// this.quantity = 0;
+
+// this.storage ={};
+const queue = {
+  quantity : 0 ,
+  storage : {}, 
+}
+}
